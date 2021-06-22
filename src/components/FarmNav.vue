ccf8a580d14b781ff9c9870ea0fdf5fb66034de9
unknown <jilaqijilaqi@qq.com>
2021-06-22 21:14:34 +0800

    added new page; changed vue config

---
+++ src/components/FarmNav.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-navigation-drawer
+    right
+    app
+  >
+    <v-list>
+      <v-list-item
+        v-for="n in 5"
+        :key="n"
+        link
+      >
+        <v-list-item-content>
+          <v-list-item-title>Item {{ n }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+    <v-divider></v-divider>
+    <v-spacer></v-spacer>
+    <v-timeline
+      dense
+    >
+      <v-timeline-item>timeline item</v-timeline-item>
+      <v-timeline-item>timeline item</v-timeline-item>
+      <v-timeline-item>timeline item</v-timeline-item>
+    </v-timeline>
+  </v-navigation-drawer>
+</template>
+
+<script>
+  export default {
+    name: 'FarmNav',
+    //
+  }
+</script>
---
+++ src/pages/NavMain/App.vue
@@ -0,0 +1,28 @@
+<template>
+<v-app>
+  <NavBar />
+  <v-main>
+  </v-main>
+</v-app>
+</template>
+
+<script>
+
+import FarmNav from '@components/FarmNav';
+
+export default {
+	name: 'NavMain',
+	components: {
+		FarmNav,
+	},
+	data: () => {
+
+	},
+	mounted () {
+
+	},
+	methods: {
+
+	},
+}
+</script>
