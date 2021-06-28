5765271e26b58cb33df3ff34559c5642124b2f78
unknown <jilaqijilaqi@qq.com>
2021-06-28 22:08:13 +0800

    test on time picker

--- src/pages/discordPage/discordPage.vue
+++ src/pages/discordPage/discordPage.vue
@@ -85,11 +85,111 @@
             height="60%"
             width="100%"
           > 
-            <div
-            ref="hc_container"
-            class="hc_container">
-            <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
-            </div>
+            <v-card
+            height="75%"
+            width="100%">
+              <div
+              ref="hc_container"
+              class="hc_container">
+              <highcharts class="hc" :options="chartOptions" ref="chart"></highcharts>
+              </div>
+            </v-card>
+            <v-card
+            height="25%"
+            width="100%">
+              <v-row>
+                <v-col
+                  cols="12"
+                  sm="2"
+                  md="2"
+                  lg="2"
+                >
+                  <v-menu
+                    ref="s_menu"
+                    v-model="menu2"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    :return-value.sync="start_time"
+                    transition="scale-transition"
+                    offset-y
+                    max-width="290px"
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="start_time"
+                        label="起始时间"
+                        prepend-icon="mdi-clock-time-four-outline"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-time-picker
+                      v-if="menu2"
+                      :allowed-minutes="allowedStep"
+                      v-model="start_time"
+                      full-width
+                      scrollable
+                      format="24hr"
+                      min="00:15"
+                      :max="end_time"
+                      @click:minute="$refs.s_menu.save(start_time)"
+                    ></v-time-picker>
+                  </v-menu>
+                </v-col>
+
+                <v-col
+                  cols="12"
+                  sm="2"
+                  md="2"
+                  lg="2"
+                >
+                  <v-menu
+                    ref="e_menu"
+                    v-model="menu3"
+                    :close-on-content-click="false"
+                    :nudge-right="40"
+                    :return-value.sync="end_time"
+                    transition="scale-transition"
+                    offset-y
+                    max-width="290px"
+                    min-width="290px"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-text-field
+                        v-model="end_time"
+                        label="结束时间"
+                        prepend-icon="mdi-clock-time-four-outline"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-text-field>
+                    </template>
+                    <v-time-picker
+                      v-if="menu3"
+                      :allowed-minutes="allowedStep"
+                      v-model="end_time"
+                      full-width
+                      scrollable
+                      :min="start_time"
+                      format="24hr"
+                      @click:minute="$refs.e_menu.save(end_time)"
+                    ></v-time-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+
+
+              <v-row>
+              </v-row>
+
+
+              <v-row>
+
+              </v-row>
+            </v-card>
+            
           </v-card>
         </v-container>
     </v-main>
@@ -110,6 +210,10 @@
     },
     data() {
       return {
+        menu2: false,
+        menu3: false,
+        end_time: "00:00",
+        start_time: "00:15",
         chartOptions: {
           series: [
             {
@@ -140,6 +244,7 @@
       })
     },
     methods: {
+      allowedStep: m => m % 15 === 0,
       get_hc_height () {
         this.$refs.chart.chart.setSize(null, this.$refs.hc_container.parentElement.clientHeight);
       }
